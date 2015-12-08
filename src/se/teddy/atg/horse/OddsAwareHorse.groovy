468eb099d79f07fc86239f1f3d72bb36a21db96e
Göran Engdahl <gengdahl@localhost.localdomain>
2015-12-08 09:04:05 +0100

    Added OddsAwareHorse and started playing with that to try and get win factor up a bit.

---
+++ src/se/teddy/atg/horse/OddsAwareHorse.groovy
@@ -0,0 +1,25 @@
+package se.teddy.atg.horse
+
+/**
+ * Created by gengdahl on 2015-12-07.
+ */
+class OddsAwareHorse extends Horse {
+    public OddsAwareHorse (def startNumber, def id, def name, def dateString,
+                            int upcomingRaceDistance, int odds){
+        super(startNumber, id, name, dateString, upcomingRaceDistance)
+        this.odds = odds
+    }
+    private int odds
+
+    @Override
+    int compareTo(Object o) {
+        def totalDiff = 0;
+        if (o instanceof OddsAwareHorse){
+            OddsAwareHorse other = (OddsAwareHorse)o
+            totalDiff = (this.averageKmTime + odds/100) - (other.averageKmTime + other.odds/100)
+        }
+        return totalDiff
+    }
+
+
+}
